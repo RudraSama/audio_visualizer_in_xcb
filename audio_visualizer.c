232240cf45cbc773dc6bede4aff8215e82bdf23e
Oscar Pavon <pavon_oscar@outlook.com>
2025-11-04 20:18:20 -0300

    restored default device

--- audio_visualizer.c
+++ audio_visualizer.c
@@ -399,7 +399,7 @@ int main(int argc, char *argv[]) {
 	snd_pcm_t *handle;
 	snd_pcm_sframes_t frames;
 
-	if ((err = snd_pcm_open(&handle, "hw:2", SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
+	if ((err = snd_pcm_open(&handle, device, SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
 		printf("Playback open error: %s\n", snd_strerror(err));
 		exit(EXIT_FAILURE);
 	}
